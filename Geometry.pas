2f9d1af521dc0589a7600a44f5b2ea6fd243e6ef
Holeur <mulkov-01@yandex.ru>
2019-01-31 21:57:34 +0300

    Update Geometry.pas
    Исправил проверку квадрата на ромб

--- Geometry.pas
+++ Geometry.pas
@@ -6,6 +6,6 @@ var
 
 begin
   read(x, y);
-  z := (x <= 6) and (x >= 2) and (y >= 2) and (x <= 6);
+  z := (x - 2 >= y - 4) and (4 - x >= y - 6) and (2 - x <= y - 4) and (x - 4 <= y - 2);
   write(z);
 end.
