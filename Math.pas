ea88782e0f4fdfe185703121bc8516e91b7795c0
Holeur <mulkov-01@yandex.ru>
2019-01-31 18:37:19 +0300

    Add files via upload

---
+++ Math.pas
@@ -0,0 +1,13 @@
+﻿program math;
+
+var
+  x, y, z: real;
+
+begin
+  Read(x, y);
+  z := 3 + (Exp(Ln(Exp(x) * (y - 1)))) / (1 + x * x * Abs(y - sin(x) / cos(x)));
+  writeln(z:1:2);
+  Read(x, y);
+  z := 1 + sin(sqrt(x + 1)) / (cos(12 * y - 4));
+  writeln(z:1:2);
+end.
