bd6759cb0e747c4e4273a1562c91b54e432e30ba
Holeur <mulkov-01@yandex.ru>
2019-02-01 08:55:38 +0300

    Update Math.pas
    Исправил числитель

--- Math.pas
+++ Math.pas
@@ -5,9 +5,10 @@ var
 
 begin
   Read(x, y);
-  z := 3 + Exp(y - 1) * (y - 1)))) / (1 + x * x * Abs(y - sin(x) / cos(x)));
+  z := (3 + Exp(y - 1)) / (1 + x * x * Abs(y - sin(x) / cos(x)));
   writeln(z:1:2);
+  
   Read(x, y);
-  z := 1 + sin(sqrt(x + 1)) / (cos(12 * y - 4));
+  z := (1 + sin(sqrt(x + 1))) / (cos(12 * y - 4));
   writeln(z:1:2);
 end.
