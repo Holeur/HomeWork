c2d8a02e306114e648beaa13009727e32d8decd8
HoleBase <mulkov-01@yandex.ru>
2019-03-02 22:16:20 +0300

    Create Kratnost.pas

---
+++ Kratnost.pas
@@ -0,0 +1,21 @@
+﻿program kratnost;
+var a:array[1..100] of integer; k,i,q,d:integer;
+begin
+for i:= 1 to 100 do //Заполняем массив рандомными числами от 0 до 50 чтобы не париться с вводом
+  a[i]:= random(50);
+writeln('Массив - '); //Выводим его для проверки
+for i:=1 to 100 do
+  write(a[i],' ');
+writeln('Введите число - ');
+read(k);
+for i:= 1 to 100 do
+  begin
+  q:= a[i] mod k;
+  if q = 0 then
+    begin
+    write(a[i],' + '); //Для проверки написал
+    d:= d + a[i];
+    end;
+  end;
+writeln(' = ',d);
+end.
