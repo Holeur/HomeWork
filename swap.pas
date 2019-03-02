676e64ffe57eed2f8668a946228516c55789a6e9
HoleBase <mulkov-01@yandex.ru>
2019-03-03 00:47:00 +0300

    Update swap.pas

--- swap.pas
+++ swap.pas
@@ -4,22 +4,25 @@ begin
 writeln('Размер массива - ');
 read(b);
 for i:=1 to b do
-  read(a[i]);
-for i:=1 to b do
   begin
+  read(a[i]);
+  end;
+min:= a[1];
+mini:= 1;
+for i:=1 to b do
   if a[i] > max then
     begin
     max:= a[i];
     maxi:= i;
-    end;
-  if a[i] < min then
-    begin
-    min:= a[i];
-    mini:=i;
-    end;
-  end;
+    end
+  else
+    if a[i] < min then
+      begin
+      min:= a[i];
+      mini:= i;
+      end;
 a[maxi]:= min;
-a[mini]:= min;
+a[mini]:= max;
 for i:=1 to b do
   write(a[i],' ');
 end.
