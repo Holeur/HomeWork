5fac9a4cd027025a93addc506d992f070c4e78b5
HoleBase <mulkov-01@yandex.ru>
2019-03-03 01:43:41 +0300

    Create checkline.pas

---
+++ checkline.pas
@@ -0,0 +1,32 @@
+﻿program checkline;
+var a:array[1..10] of array [1..10] of integer; i,u,k,n:integer;
+begin
+Writeln('Размер матрицы и номер строки сверху');
+read(n,k);
+for i:=1 to n do
+  for u:=1 to n do
+    a[i,u]:= random(1000);
+for i:=1 to n do //Весь этот фор рисует матрицу, много, зато красиво
+  begin
+  writeln('');
+  for u:=1 to n do
+    begin
+    if a[i,u] > 99 then
+      write(a[i,u],' ')
+    else
+      if (a[i,u] > 9) and (a[i,u] < 100) then
+        write(a[i,u],'  ')
+      else
+        write(a[i,u],'   ');
+    end;
+  end;
+writeln(' ');
+writeln('--------------------------');
+for i:=1 to n do
+  if i = k then
+    for u:=1 to n do
+      if u = k then
+        write('\\\',a[i,u],'\\\ ')
+      else
+        write(a[i,u],' ');
+end.
