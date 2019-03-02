c88f06a566c4e735d0c32f8f1a04cfa81300f7ed
HoleBase <mulkov-01@yandex.ru>
2019-03-02 22:16:22 +0300

    Create upper.pas

---
+++ upper.pas
@@ -0,0 +1,21 @@
+﻿program upper;
+var a:array[1..10] of integer; i,b,c,d:integer;
+begin
+d:=0; 
+write('Размер массива - ');
+read(b);
+for i:=1 to b do
+  read(a[i]);
+for i:=1 to b do
+  if a[i] > c then
+    c:= a[i]
+  else
+    begin
+    writeln('Массив не возрастающий');
+    d:= 1; //Поднимаем флаг чтобы вторая фраза не появилась в результате
+    break
+    end;
+if d = 0 then
+  writeln('Массив возрастающий');
+end.
+
