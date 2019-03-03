35c499ba991c2c05cf7d7ff8927ef0a9a258b811
HoleBase <mulkov-01@yandex.ru>
2019-03-03 19:17:19 +0300

    Update upper.pas
    Исправил отрицательные числа

--- upper.pas
+++ upper.pas
@@ -7,13 +7,18 @@ read(b);
 for i:=1 to b do
   read(a[i]);
 for i:=1 to b do
-  if a[i] > c then
+  if i = 1 then 
     c:= a[i]
   else
     begin
-    writeln('Массив не возрастающий');
-    d:= 1; //Поднимаем флаг чтобы вторая фраза не появилась в результате
-    break
+    if a[i] > c then
+      c:= a[i]
+    else
+      begin
+      writeln('Массив не возрастающий');
+      d:= 1; //Поднимаем флаг чтобы вторая фраза не появилась в результате
+      break
+      end;
     end;
 if d = 0 then
   writeln('Массив возрастающий');
